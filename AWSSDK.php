3f8f5edd3123821f0084731503b9f35708de19a8
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-30 23:23:20 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: I8ae79f806e7a988f926e5705c16cb283c37893ee

--- AWSSDK.php
+++ AWSSDK.php
@@ -6,11 +6,12 @@ $wgExtensionCredits['other'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'AWSSDK',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:AWSSDK',
-	'version'        => '2.1.0',
+	'version'        => '2.2.0',
 	'author'         => array( '[//github.com/aws/aws-sdk-php Amazon]', '[//twitter.com/thaiphan1990 Thai Phan]' ),
 	'descriptionmsg' => 'awssdk-desc'
 );
 
+$wgMessagesDirs['AWSSDK'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['AWSSDK'] = $dir . 'AWSSDK.i18n.php';
 
 require_once( $dir . 'vendor/autoload.php' );
